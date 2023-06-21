dbcb42a55b0ce7a706b1d532df63d9edd47b7007
tgsmith-17 <gagesmith369@gmail.com>
2023-06-21 00:09:51 -0400

    changing some stuff around

--- src/Pages/describer_pages/Home.js
+++ src/Pages/describer_pages/Home.js
@@ -4,16 +4,29 @@ import Header from "./Header";
 function Home()
 {
   const [quote, setQuote] = useState("It will display here");
+  const [author, setAuthor] = useState("Author");
   
   const getQuote = async () => {
     try {
       var data = await (
-        await fetch("https://api.kanye.rest")
+        await fetch(`https://type.fit/api/quotes`)
       ).json();
-      
-      setQuote(data.quote);
+
+      let tmp = Object.keys(data).length;
+
+      let selected = data[Math.floor(Math.random()*tmp)];
+
+      setQuote(selected.text);
+
+      let tmpAuth = author;
+      setAuthor(selected.author);
+
+      if(author === tmpAuth) {
+        setAuthor("Unknown");
+      }
     } catch(e) {
       setQuote("Unable to fetch quote. Please try again later");
+      console.log(e);
     }
   };
 
@@ -24,9 +37,9 @@ function Home()
         <h1>Home Page</h1>
         <div className="body">
           <div className="space"></div>
-          <h3>Here's a random Kanye West quote</h3>
+          <h3>Fetch an Inspirational Quote</h3>
           <h2>"{quote}"</h2>
-          <h3>-- Kanye West</h3>
+          <h3 className="author">-- {author}</h3>
           <button onClick={getQuote}>Get a Quote</button>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <React.StrictMode>
-    <Router basename='my-portfolio/'>
+    <Router basename='/my-portfolio'>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />} />
