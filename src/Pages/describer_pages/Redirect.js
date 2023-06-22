64a0932d2134a7aa1f11cacaa2d806a399818f02
tgsmith-17 <gagesmith369@gmail.com>
2023-06-22 11:43:13 -0400

    finished redirect page

--- src/Pages/describer_pages/Redirect.js
+++ src/Pages/describer_pages/Redirect.js
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Redirect()
 {
   
   return(
-    <div className="error-page">
-      <h1>Sorry!</h1>
-      <h2>As far as I'm aware, GitHub Pages doesn't (technically) allow having multiple routes, so this page might come up a lot :/</h2>
-      <h2>But don't fret!</h2>
-      <h2>You'll be redirected to the Home page with a click of the button below :)</h2>
+    <div className="padding">
+      <div className="error-page">
+        <h1>Sorry!</h1>
+        <h2>As far as I'm aware, GitHub Pages doesn't (technically) allow having multiple routes, so this page might come up a lot :/</h2>
+        <h2>But don't fret!</h2>
+        <h2>You'll be redirected to the Home page with a click of the button below :)</h2>
+        <button className="redirect-button"><Link to={'/'}>Home Page</Link></button>
+      </div>
     </div>
   );
 };
